23be752af78a421cfd2addb8aa6d507a82dbb91b
Davide Angelocola <davide.angelocola@gmail.com>
2011-09-25 11:54:48 +0200

    ref: description

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject uritemplate "1.0.0-SNAPSHOT"
-  :description "Uri templates for clojure."
+  :description "Uri templates (type 1) based on the draft
+  specification published on the IETF site. See
+  http://tools.ietf.org/html/draft-gregorio-uritemplate-06"
   :dependencies [[org.clojure/clojure "1.3.0"]])
