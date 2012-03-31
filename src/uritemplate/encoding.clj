673246a8f9f0d4fa5fcd26e35395c6e129d483d7
Davide Angelocola <davide.angelocola@gmail.com>
2012-03-31 10:59:34 +0200

    ref: removed old stuff

--- src/uritemplate/encoding.clj
+++
@@ -1,7 +0,0 @@
-(ns uritemplate.encoding)
-
-(defn url-encode [value]
-  "A thin wrapper around standard Java urlencoder that always uses
-utf8 on the string representation of the specified value."
-  (assert (not (nil? value)))
-  (java.net.URLEncoder/encode (str value) "utf8"))
