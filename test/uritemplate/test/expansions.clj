09b70ffafaf0e8cf6f1ce4cd8bef690675650efd
Davide Angelocola <davide.angelocola@gmail.com>
2012-04-01 13:19:24 +0200

    ref: naming

--- test/uritemplate/test/expansions.clj
+++ test/uritemplate/test/expansions.clj
@@ -2,15 +2,19 @@
   (:use [uritemplate.expansions])
   (:use [clojure.test]))
 
-(deftest render-smoke-tests
+(deftest urlencode-test
+  (is (= "hello" (urlencode "hello")))
+  (is (= "hello%20world%21" (urlencode "hello world!"))))
+
+(deftest render-test
   (is (= "/1,2,3" (render "/" "," '(1 2 3))))
   (is (= "/foo" (render "/" "" "foo"))))
 
-(deftest truncate-to-smoke-test
+(deftest truncate-to-test
   (is (= "val" (truncate-to "value" 3)))
   (is (= "value" (truncate-to "value" 5)))
   (is (= "value" (truncate-to "value" 9999))))
 
-(deftest simple-string-expansion 
+(deftest simple-expansion-test
   (is (= "val" (expand {:type :simple :vars [{:name "var" :maxlen 3}]}
                        {:var "value"}))))
