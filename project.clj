48bfdc293cb288ca5cc76632b7d536a7425beef3
Davide Angelocola <davide.angelocola@emaze.net>
2012-05-10 14:08:02 +0200

    enh: imported tests from https://github.com/uri-templates/uritemplate-test

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject uritemplate "1.0.0-SNAPSHOT"
   :description "Uri templates implmentation based on RFC 6570."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"] 
+                [org.clojure/data.json "0.1.2" :scope "test"]])
 
