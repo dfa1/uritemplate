542e313209ea595c4eb17c7ab179764f3f5bc724
Davide Angelocola <davide.angelocola@gmail.com>
2013-01-26 11:06:38 +0100

    new version: 1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uritemplate "1.0.0"
+(defproject uritemplate "1.1-SNAPSHOT"
   :description "Uri templates implementation (fully compatible with RFC 6570)"
   :dependencies [[org.clojure/clojure "1.4.0"] 
                 [org.clojure/data.json "0.1.2" :scope "test"]])
