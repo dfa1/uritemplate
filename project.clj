e627e31112bb0768ee736f5668b24fd6bb9fd802
Davide Angelocola <davide.angelocola@gmail.com>
2013-01-26 11:03:43 +0100

    fix: project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject uritemplate "1.0.0-SNAPSHOT"
-  :description "Uri templates implmentation based on RFC 6570."
+  :description "Uri templates implementation (fully compatible with RFC 6570)"
   :dependencies [[org.clojure/clojure "1.4.0"] 
                 [org.clojure/data.json "0.1.2" :scope "test"]])
 
