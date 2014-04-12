b23d0d122ab5b3fe2618971d3c71eb9de4eb34d2
Davide Angelocola <davide.angelocola@gmail.com>
2014-04-12 14:37:21 +0200

    dev: bumped clojure to 1.6

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject uritemplate "1.1-SNAPSHOT"
   :description "Clojure implementation of the URI Template (RFC 6570), level 4 complaint."
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                 [org.clojure/data.json "0.1.2" :scope "test"]])
 
