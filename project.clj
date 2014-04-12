9b5c2aed511f03060c9ad6cf831b47fe9f41caf9
Davide Angelocola <davide.angelocola@gmail.com>
2014-04-12 14:36:43 +0200

    ehn: documentation

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject uritemplate "1.1-SNAPSHOT"
-  :description "Uri templates implementation (fully compatible with RFC 6570)"
-  :dependencies [[org.clojure/clojure "1.4.0"] 
+  :description "Clojure implementation of the URI Template (RFC 6570), level 4 complaint."
+
+  :dependencies [[org.clojure/clojure "1.4.0"]
                 [org.clojure/data.json "0.1.2" :scope "test"]])
 
