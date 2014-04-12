0bd5442c81a4f4999d3cd1137b2c9fdb6cff0ba4
Davide Angelocola <davide.angelocola@gmail.com>
2014-04-12 15:33:37 +0200

    fix: typo

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject uritemplate "1.2-SNAPSHOT"
-  :description "Clojure implementation of the URI Template (RFC 6570), level 4 complaint."
-
+  :description "Clojure implementation of the URI Template (RFC 6570), level 4 compliant."
   :dependencies [[org.clojure/clojure "1.6.0"]
                 [org.clojure/data.json "0.1.2" :scope "test"]])
 
