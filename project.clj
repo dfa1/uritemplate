5b6c4379ddf19dc10c77af97dc8a4bf1c10d7837
Davide Angelocola <davide.angelocola@gmail.com>
2014-04-12 15:32:35 +0200

    new version: 1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uritemplate "1.1"
+(defproject uritemplate "1.2-SNAPSHOT"
   :description "Clojure implementation of the URI Template (RFC 6570), level 4 complaint."
 
   :dependencies [[org.clojure/clojure "1.6.0"]
